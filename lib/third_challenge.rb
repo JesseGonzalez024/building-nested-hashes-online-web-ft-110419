9dfca93d503499aa4cb47d88e322a383cd4292c3
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-10 01:36:32 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,7 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {"Lord Montague", age: "53"},
+      :patriarch => {"Lord Montague" age: "53"},
       :matriarch => {"Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
